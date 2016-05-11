65b810822dbcf4b9df38b693b9a2e260b56cf26e
Ryeker J Herndon <ryekerjherndon@Ryekers-MacBook-Pro.local>
2016-05-10 20:45:00 -0700

    added date js and onscroll js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 (function ($, root, undefined) {
 	$(function () {
 		'use strict';
-		
+		window.addEventListener("scroll",function() {
+		   if(window.scrollY > 2300) {
+		      $('.past-jobs').slideDown("slow");
+		   }
+		   else {
+		      $('.past-jobs').slideUp("slow");
+		   }
+		},false);
+
 	});
 })(jQuery, this);
